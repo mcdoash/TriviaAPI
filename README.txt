8cf3a1886ae64d9b2148c2bd3c1cdd0517909a25
mcdoash <mcdoash@users.noreply.github.com>
2019-11-04 22:04:02 -0500

    Mostly working?

---
+++ README.txt
@@ -0,0 +1,7 @@
+If an invalid data for parameter id given (i.e. difficulty not an integer or less than 1/greater than 3) then the parameter is set to null.
+
+
+Required Packages
+    * express
+    * uuid
+    * fs
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const uuid = require("uuid/v4");
 const fs = require('fs');
+
+//define folder paths
 const qFolder = "./questions";
 const sessionFolder = "./sessions";
 
@@ -17,21 +19,36 @@ app.get("/questions", getQuestions);
 app.get("/questions", sendQuestions);
 
 app.post("/sessions", newSession);
+app.get("/sessions", getSessions);
+app.delete("/sessions/:sessionid", deleteSession);
 
+//general 404
 
 /*
 
 */
 function queryParser(req, res, next){
-    if(!req.query.limit){
-		req.query.limit = 10; //default
+    //set to default 10 if limit not specified or < 1
+    if(!req.query.limit || req.query.limit < 1){
+		req.query.limit = 10; 
 	}
+    //difficulty not specified, set to null
     if(!req.query.difficulty){
 		req.query.difficulty = null;
 	}
+    //invalid difficulty if not an integer or not 1-3
+    else if (!Number.isInteger(req.query.difficulty) || req.query.difficulty < 1  req.query.difficulty > 3) {
+        req.query.difficulty = null;
+    }
+    //category not specified, set to null
     if(!req.query.category){
 		req.query.category = null;
 	}
+    //invalid category if not an integer or not 1-24
+    else if (!Number.isInteger(req.query.category) || req.query.category < 1  req.query.category > 24) {
+        req.query.category = null;
+    }
+    //token not specified, set to null
     if(!req.query.token){
 		req.query.token = null;
 	}
@@ -43,6 +60,7 @@ function queryParser(req, res, next){
 Checks to see if requested token is valid, if it exists
 */
 function isValidToken(req, res, next) {
+    //if token was given, check if valid
     if(req.query.token != null) {
         let validToken = false;
         let tokenID = req.query.token + ".json"; //to match file names
@@ -77,85 +95,88 @@ function getQuestions(req, res, next) {
     }
     //otherwise get questions
     else {
-        //get all files in directory
-        fs.readdirSync(qFolder, function(err, files) {
-            //create a random starting point
-            let randIndex = Math.floor(Math.random() * (files.length-limit)) + 1;
-            let i = randIndex;
+        //get array of all question files
+        let files = fs.readdirSync(qFolder);
+        
+        //create a random starting point
+        //let randIndex = Math.floor(Math.random() * (files.length-limit)) + 1;
+        let randIndex = 375;
+        let i = randIndex;
+        
+        //RANDOM QUESTION
 
-            //iterate through files in directory until enough questions are gathered
-            while(questions.length < limit) {
-                //get and parse data from file at index i
-                let file = qFolder + "/" + files[i];
-                let data = fs.readFileSync(file);
-                let q = JSON.parse(data);
+        //iterate through files in directory until enough questions are gathered
+        while(questions.length < limit) {
+            //get and parse data from file at index i
+            let file = qFolder + "/" + files[i];
+            let data = fs.readFileSync(file);
+            let q = JSON.parse(data);
 
-                //add question by default
-                let addQ = true;
+            //add question by default
+            let addQ = true;
 
-                //check for matching difficulty
-                if(difficulty != null) {
-                    if(difficulty != q.difficulty_id) {
+            //check for matching difficulty
+            if(difficulty != null) {
+                if(difficulty != q.difficulty_id) {
+                    addQ = false;
+                }
+            }
+            //check for matching category
+            if(category != null) {
+                if(category != q.category_id) {
+                    addQ = false;
+                }
+            }
+            //handle session
+            if(token != null) {
+                file = sessionFolder + "/" + token + ".json";
+                data = fs.readFileSync(file);
+
+                //get array of questions already recieved
+                let qRecieved = JSON.parse(data);
+                
+                //check of this question has already been recieved, don't add if so
+                for(let j=0; j<qRecieved.questions.length; j++) {
+                    if(qRecieved.questions[j] == files[i]) {
                         addQ = false;
-                    }
-                }
-                //check for matching category
-                if(category != null) {
-                    if(category != q.category_id) {
-                        addQ = false;
-                    }
-                }
-                //handle session
-                if(token != null) {
-                    file = sessionFolder + "/" + token + ".json";
-                    data = fs.readFileSync(file);
-                    
-                    //get array of questions already recieved
-                    let qRecieved = JSON.parse(data);
-                    
-                    //check of this question has already been recieved, don't add if so
-                    for(let i=0; i<qRecieved.questions.length; i++) {
-                        if(qRecieved.questions[i] == files[i]) {
-                            addQ = false;
-                            break;
-                        }
-                    }
-                    
-                    //add question to session
-                    if(addQ) {
-                        qRecieved.questions.push(files[i]);
-                        fs.writeFileSync(file, JSON.stringify(qRecieved));
+                        break;
                     }
                 }
 
-                //add to question array if all clear
+                //add question to session
                 if(addQ) {
-                    questions.push(q)
+                    qRecieved.questions.push(files[i]);
+                    fs.writeFileSync(file, JSON.stringify(qRecieved));
                 }
+            }
 
-                //increase index if not at end
-                if(i < (files.length-1)) {
-                    i++;
-                }
-                //loop back to the beginning if at end
-                else {
-                    i = 0;
-                }
-                //if looped through all files, break
-                if(i == randIndex) {
-                    break;
-                }
+            //add to question array if all clear
+            if(addQ) {
+                questions.push(q)
             }
-            //not enough questions
-            if(questions.length < limit) {
-                res.status = 1;
+
+            //increase index if not at end
+            if(i < (files.length-1)) {
+                i++;
             }
-            //everything ok
+            //loop back to the beginning if at end
             else {
-                res.status = 0;
-                res.questions = questions;
+                i = 0;
             }
-        });
+            //if looped through all files, break
+            if(i == randIndex) {
+                break;
+            }
+        }
+        //not enough questions
+        if(questions.length < limit) {
+            res.status = 1;
+        }
+        //everything ok
+        else {
+            res.status = 0;
+            res.questions = questions;
+        }
     }
     next();
 }
@@ -182,17 +203,68 @@ Creates a new session by creating a new file to hold session questions
 */
 function newSession(req, res, next) {
     //create unique id
-    let tokenID = uuid().toString() 
+    let tokenID = uuid().toString();
     
     //write new file
     let file = sessionFolder + "/" + tokenID + ".json";
-    let content = {questions: []};
+    let content = {token: tokenID, questions: []};
     fs.writeFileSync(file, JSON.stringify(content));
+    
+    console.log("Created new session " + tokenID);
     
     //send response
     res.writeHead(201, {"Content-Type": "text/plain"});
     res.write(tokenID);
     res.end();
+    next();
+}
+
+
+/*
+
+*/
+function getSessions(req, res, next) {
+    let content = [];
+    
+    fs.readdirSync(sessionFolder).forEach(function(file) {
+        try {
+            let data = fs.readFileSync(sessionFolder + "/" + file);
+            let session = JSON.parse(data);
+
+            content.push(session.token);
+        }
+        catch(err) {
+            console.log(err);
+        }
+        //.DS_Store file
+    });
+
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(JSON.stringify(content));
+    res.end();
+    next();
+}
+
+
+/*
+Deletes a specified session
+*/
+function deleteSession(req, res, next) {
+    let token = req.params.sessionid;
+    let file = sessionFolder + "/" + token + ".json";
+        
+    //try removing id in request
+    try {
+        fs.unlinkSync(file);
+        console.log("Session " + token + " removed")
+        
+        //send success status
+        res.status(200).end();
+    }
+    //session did not exist, send 404
+    catch(err) {
+        res.status(404).end();
+    }
     next();
 }
 
